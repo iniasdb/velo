e186fb4adbabbe55dcc6611b05911f99d818b834
iniasdb <inias.debelder@hotmail.be>
2022-06-06 18:01:42 +0200

    version: 2.3
    ======================== CODE =========================
    
    FEATURES
    ----------------------
    
    > Satic Site Generator
        - Created bike and user template
        - Added CSS styling
    
    CHANGES
    ----------------------
    
    ==================== REMARKS & NOTES ==================

--- helperClasses/SiteGenerator.py
+++ helperClasses/SiteGenerator.py
@@ -1,4 +1,4 @@
-from jinja2 import Environment, FileSystemLoader 
+from jinja2 import Environment, FileSystemLoader
 
 class SiteGenerator:
     def __init__(self):
@@ -13,6 +13,10 @@ class SiteGenerator:
         else:
             in_use = "red"
 
+        number = ""
+        if not station.number == None:
+            number = station.number
+
         slots = ""
         for slot in station.slots_list:
             if slot.get_occupied():
@@ -25,7 +29,6 @@ class SiteGenerator:
 
             of.write(
                 self.station_template.render(
-                    stationName= station.name,
                     street=station.street,
                     number=station.number,
                     postalCode=station.postal_code,
@@ -36,3 +39,54 @@ class SiteGenerator:
                 )
             )
 
+    def create_bikepage(self, bike):
+        if bike.in_use:
+            color = "green"
+            in_use = "in use"
+        else:
+            color = "red"
+            in_use = "not in use"
+
+        with open("_site/bikes.html", "w") as of:
+
+            of.write(
+                self.bike_template.render(
+                    uid = bike.uid,
+                    inUse = in_use,
+                    color = color
+                )
+            )
+
+    def create_userpage(self, user):
+        with open("_site/users.html", "w") as of:
+            subscribed = ""
+            if not user.subscribed:
+                subscribed = "not "
+
+            bike_list = "<ul>"
+            transporter = ""
+            if not user.transporter:
+                transporter = "not "
+            else:
+                user_bike_list = user.get_bike_list()
+
+                for bike in user_bike_list:
+                    bike_list += "<li>"+ str(bike.uid) + "</li>"
+
+                if len(user_bike_list) == 0:
+                    bike_list += "<p>No bike equiped</p>"
+                else:
+                    bike_list += "</ul>"
+
+            
+            of.write(
+                self.user_template.render(
+                    fname = user.fname.capitalize(),
+                    lname = user.lname.capitalize(),
+                    uid = user.uid,
+                    email = user.email,
+                    subscribed = subscribed,
+                    transporter = transporter,
+                    bikeList = bike_list
+                )
+            )
